c03ab54c12609034fea379d3c24357ac184a3453
Adam Polický <adam.policky@gmail.com>
2025-06-24 21:04:31 +0200

    wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ["./src/**/*.{html,js}", "./index.html"],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                "metron": ["Metron", ...defaultTheme.fontFamily.mono]
+            }
+        },
     },
     plugins: [],
 };
